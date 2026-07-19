369f184a92e4c4f3fea97b3d56a0a94879a5a722
Rokhaya SALL <sall.nrd69@gmail.com>
2026-07-19 21:50:30 +0200

    docker / déploiement

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ const PORT = process.env.PORT || 3000;
 // ─────────────────────────────
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
